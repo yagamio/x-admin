418df4f6b15a75131f5e1a9d0ed03986e5fa98df
cheng <zhicheng.he@outlook.com>
2023-06-30 21:20:32 +0200

    Updates

--- src/main/java/com/hb/sys/controller/UserController.java
+++ src/main/java/com/hb/sys/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.hb.sys.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hb.common.vo.Result;
 import com.hb.sys.entity.User;
 import com.hb.sys.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.Controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -55,4 +58,30 @@ public class UserController {
         return Result.success();
     }
 
+    @GetMapping("/list")
+    public Result<Map<String, Object>> getUserList(@RequestParam(value = "username", required = false) String username,
+                                                   @RequestParam(value = "phone", required = false) String phone,
+                                                   @RequestParam(value = "pageNo") Long pageNo,
+                                                   @RequestParam(value = "pageSize") Long pageSize) {
+
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(StringUtils.hasLength(username), User::getUsername, username);
+        wrapper.eq(StringUtils.hasLength(phone), User::getPhone, phone);
+        wrapper.orderByDesc(User::getId);
+
+        Page<User> page = new Page<>(pageNo, pageSize);
+        userService.page(page, wrapper);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("total", page.getTotal());
+        data.put("rows", page.getRecords());
+
+        return Result.success(data);
+    }
+
+    @PostMapping
+    public Result<?> addUser(@RequestBody User user) {
+        userService.save(user);
+        return Result.success("User successfully added");
+    }
 }
